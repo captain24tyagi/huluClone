ef0a0fb82d3293b98a9e6ab41a330241832b4855
captain24tyagi <sarthak2002tyagi@gmail.com>
2023-01-02 22:24:36 +0530

    first commit

---
+++ components/Header.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import HeaderItems from "./HeaderItems";
+import{
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+}
+from "@heroicons/react/outline";
+function Header() {
+  return (
+    <header className="flex flex-col md:flex-row m-5 justify-between items-center">
+        <div className="flex flex-grow justify-evenly max-w-2xl">
+            <HeaderItems 
+                title='HOME'
+                Icon={HomeIcon}
+            />
+            <HeaderItems 
+                title='TRENDING'
+                Icon={LightningBoltIcon}
+            />
+            <HeaderItems 
+                title='VERIFIED'
+                Icon={BadgeCheckIcon}
+            />
+            <HeaderItems 
+                title='COLLECTIONS'
+                Icon={CollectionIcon}
+            />
+            <HeaderItems 
+                title='SEARCH'
+                Icon={SearchIcon}
+            />
+            <HeaderItems 
+                title='ACCCOUNT'
+                Icon={UserIcon}
+            />
+        </div>
+        <Image
+            className="object-contain text-blue-600"
+            src="https://links.papareact.com/ua6"
+            width={200}
+            height={100}
+            alt=''
+        />
+    </header>
+  )
+}
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+
